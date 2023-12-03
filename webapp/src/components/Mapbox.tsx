8b3a50d1d7bbd04b726253197f98fde117b00e73
Owen Cheung <ocheung198@gmail.com>
2023-12-02 23:59:26 -0800

    comment out map

--- webapp/src/components/Mapbox.tsx
+++ webapp/src/components/Mapbox.tsx
@@ -15,70 +15,72 @@ const Mapbox: React.FC = () => {
       zoom: 13,
     });
 
-    // Fog Harbor
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-122.41025, 37.80889])
-      .addTo(map);
+    // fake map code
 
-    // Hog Island
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-122.393303, 37.795831])
-      .addTo(map);
+    // // Fog Harbor
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([-122.41025, 37.80889])
+    //   .addTo(map);
 
-    // Liholiho Yacht Club
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-122.41450866001664, 37.78843914681705])
-      .addTo(map);
+    // // Hog Island
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([-122.393303, 37.795831])
+    //   .addTo(map);
 
-    // House of Prime Rib
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-122.4225, 37.79338])
-      .addTo(map);
+    // // Liholiho Yacht Club
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([-122.41450866001664, 37.78843914681705])
+    //   .addTo(map);
 
-    // Kokkari Estiatorio
-    var marker = new mapboxgl.Marker()
-      .setLngLat([-122.399661, 37.796996])
-      .addTo(map);
+    // // House of Prime Rib
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([-122.4225, 37.79338])
+    //   .addTo(map);
 
-    var geojson = {
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          properties: {},
-          geometry: {
-            coordinates: [
-              [-122.41580962523275, 37.782433947124346],
-              [-122.41850156234553, 37.79630265687568],
-              [-122.4103222149648, 37.797407132462666],
-              [-122.41089166320018, 37.8009249816739],
-              [-122.41694852170355, 37.80513799919643],
-              [-122.41751796993896, 37.80808287843996],
-              [-122.41265177592759, 37.80849188015806],
-            ],
-            type: "LineString",
-          },
-        },
-      ],
-    };
-    map.on("load", function () {
-      map.addLayer({
-        id: "line",
-        type: "line",
-        source: {
-          type: "geojson",
-          data: geojson,
-        },
-        layout: {
-          "line-join": "round",
-          "line-cap": "round",
-        },
-        paint: {
-          "line-color": "#7e5bef",
-          "line-width": 5,
-        },
-      });
-    });
+    // // Kokkari Estiatorio
+    // var marker = new mapboxgl.Marker()
+    //   .setLngLat([-122.399661, 37.796996])
+    //   .addTo(map);
+
+    // var geojson = {
+    //   type: "FeatureCollection",
+    //   features: [
+    //     {
+    //       type: "Feature",
+    //       properties: {},
+    //       geometry: {
+    //         coordinates: [
+    //           [-122.41580962523275, 37.782433947124346],
+    //           [-122.41850156234553, 37.79630265687568],
+    //           [-122.4103222149648, 37.797407132462666],
+    //           [-122.41089166320018, 37.8009249816739],
+    //           [-122.41694852170355, 37.80513799919643],
+    //           [-122.41751796993896, 37.80808287843996],
+    //           [-122.41265177592759, 37.80849188015806],
+    //         ],
+    //         type: "LineString",
+    //       },
+    //     },
+    //   ],
+    // };
+    // map.on("load", function () {
+    //   map.addLayer({
+    //     id: "line",
+    //     type: "line",
+    //     source: {
+    //       type: "geojson",
+    //       data: geojson,
+    //     },
+    //     layout: {
+    //       "line-join": "round",
+    //       "line-cap": "round",
+    //     },
+    //     paint: {
+    //       "line-color": "#7e5bef",
+    //       "line-width": 5,
+    //     },
+    //   });
+    // });
     // Add any additional customization or features here
 
     // Cleanup the map when the component is unmounted
