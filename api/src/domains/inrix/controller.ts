43aaf327ace3cd54b58e22b6f5fd90e771f3d779
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-02 17:20:28 -0800

    endpoints

---
+++ api/src/domains/inrix/controller.ts
@@ -0,0 +1,27 @@
+
+  
+//   
+import generateHash from "../../util/generateHash";
+
+export async function inrixapi() {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+    const response = await fetch("https://api.iq.inrix.com/auth/v1/appToken?appId=rx234ow9q7&hashToken=cngyMzRvdzlxN3w1bXFiR2F0aDF0YVpWVWNoZzdhVlE5bmpEOUlRMXlQM2FlcXNxWTUx")
+        .then(response => response.text())
+        .then(result => { return result })
+        .catch(error => console.log('error', error));
+
+    return response;
+}
+
+export function getMessage() {
+    const helloFrom = {
+      message: `Hello World!`,
+      id: generateHash(),
+    };
+  
+    return helloFrom;
+  }
+  
--- api/src/domains/inrix/routes.ts
+++ api/src/domains/inrix/routes.ts
@@ -1,5 +1,16 @@
 import express, { Request, Response } from "express";
-
+import { getMessage } from "./controller";
+import { inrixapi } from "./controller";
 const router = express.Router();
 
+
+router.get('/about', async (req, res) => {
+
+    const response = await inrixapi();
+
+    console.log(response);
+    res.send(response);
+  });
+
 export default router;
+
